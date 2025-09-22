e85ebc45850e0273e05621d1e91bbdd7f24cd82e
sades <hsolistorrijos@gmail.com>
2025-09-22 17:17:01 +0200

    Add conversion functionality to unit converter

--- Actividades/Tareas/Ejercicio24/conversor_unidades.py
+++ Actividades/Tareas/Ejercicio24/conversor_unidades.py
@@ -1,6 +1,21 @@
 numero = int(input("Inserta el valor"))
 medida = input("En que medida esta el valor(km, hm, dam, m,dm,cm,mm)")
-conversion = input("A que medida quiere convertir")
+
 
 match medida:
-    case km
+    case "km":
+        print("Es igual a", numero * 1000, "m")
+    case "hm":
+        print("Es igual a", numero * 100, "m")
+    case "dam":
+        print("Es igual a", numero * 10, "m")
+    case "m":
+        print("Es igual a", numero, "m")
+    case "dm":
+        print("Es igual a", numero / 10, "m")
+    case "cm":
+        print("Es igual a", numero / 100, "m")
+    case "mm":
+        print("Es igual a", numero / 1000, "m")
+    case _:
+        print("Medida no valida")
