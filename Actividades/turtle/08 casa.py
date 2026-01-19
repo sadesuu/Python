610696b99d0a3aebf99af732eff0ea0870b1f927
sadesuu <hsolistorrijos@gmail.com>
2026-01-19 19:32:48 +0100

    m

---
+++ Actividades/turtle/08 casa.py
@@ -0,0 +1,11 @@
+import turtle as t
+
+t.Turtle()
+for _ in range(4): 
+    t.forward(100) # para delante (número de pasos)
+    t.right(90)
+for _ in range(3):
+    t.forward(100)
+    t.left(120)
+
+t.mainloop()
