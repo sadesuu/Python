9021efa275790d6469a3dff1f0a0d6a11d2d25cd
sadesuu <hsolistorrijos@gmail.com>
2026-01-21 20:28:56 +0100

    Add functionality to decrement the counter in tkinter application

--- Actividades/tkinter/03_contador.py
+++ Actividades/tkinter/03_contador.py
@@ -7,11 +7,21 @@ def sumar():
     contador +=1
     label.config(text=contador)
 
+
+def restar():
+    global contador
+    contador -=1
+    label.config(text=contador)
+
+
 ventana = tk.Tk()
 label = tk.Label(ventana, text=contador)
 label.pack()
 
 boton = tk.Button(ventana, text="+1", command=sumar)
+boton.pack()
+
+boton = tk.Button(ventana, text="-1", command=restar)
 boton.pack()
 
 ventana.mainloop()
